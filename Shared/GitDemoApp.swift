8d41d92872645ff075dc0a22a89b314c8f31c493
Arthur Davtian <arthurdavtian@MacBook-Pro-Arthur.local>
2021-06-22 01:10:08 +0300

    Initial commit

---
+++ Shared/GitDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitDemoApp.swift
+//  Shared
+//
+//  Created by Arthur Davtian on 22.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct GitDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
